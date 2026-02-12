08053011d40e20fe63f57a001dcb81a1949edb88
pupkin-afk <ma5400026@gmail.com>
2026-02-12 09:04:58 +0200

    fixes

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,9 +1,9 @@
 export const AppConfig = {
     ACCESS_TOKEN_AGE: 30 * 24 * 3600 * 1000,
     REFRESH_TOKEN_AGE: 30 * 24 * 3600 * 1000,
-    originURL: "http://localhost:5500",
+    originURL: process.env.ORIGIN_URL || "http://localhost:5500",
     minio: {
-        bucket: "geogrambucket"
+        bucket: process.env.MINIO_BUCKET || "geogrambucket"
     },
     ratios: {
         avatar_ratio: 1 / 1,
